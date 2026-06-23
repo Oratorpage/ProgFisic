f2c0037de7991c6b99ccb1f21ab5ade170c557d6
Andrea <fantezombie150@gmail.com>
2026-06-23 21:55:45 +0200

    And the fun 'complete this in the most elegant way' moment begins

--- src/vChange.cpp
+++ src/vChange.cpp
@@ -6,70 +6,12 @@ namespace flock {
 
 // I discovered that I could have used std::hypot, would have needed to elevate
 // it to pow 2 but maybe better?
+// this whole part kinda sucks, I was terribly burnt out and I tried to push
+// forward, let's redo it
 double distSq(V2D const& a, V2D const& b) {
   double dx{a.x - b.x};
   double dy{a.y - b.y};
   return dx * dx + dy * dy;
-}
-// Adesso funziona correttamente
-double normGammaHorizon(Boid const& bi) {
-  double gamma{0.};
-  constexpr double PI{3.14159265358979323846264338327950288};
-  // First quadrant and second
-  if ((bi.Vel().x > 0. && bi.Vel().y < 0.) ||
-      (bi.Vel().x < 0. && bi.Vel().y < 0.)) {
-    gamma = -std::atan2(bi.Vel().y, bi.Vel().x) * 180. / PI;
-  }
-  // Third quadrant and fourth
-  else if ((bi.Vel().x < 0. && bi.Vel().y > 0.) ||
-           (bi.Vel().x > 0. && bi.Vel().y > 0.)) {
-    gamma = 360. - std::atan2(bi.Vel().y, bi.Vel().x) * 180. / PI;
-  }  // Special case
-  else if (bi.Vel().x > 0. && bi.Vel().y == 0.) {
-    gamma = 0.;
-  } else if (bi.Vel().x == 0. && bi.Vel().y < 0.) {
-    gamma = 90.;
-  } else if (bi.Vel().x < 0. && bi.Vel().y == 0.) {
-    gamma = 180.;
-  } else if (bi.Vel().x == 0. && bi.Vel().y > 0.) {
-    gamma = 270.;
-  } else if (bi.Vel().x == 0. && bi.Vel().y == 0.) {
-    gamma = 0.;
-  } else {
-    throw std::runtime_error(
-        "The velocity of the boid was not a compatible value to deduce an "
-        "angle with the horizon (counter-clockwise)");
-  }
-  return gamma;
-}
-// Questo adesso funziona correttamente
-double normAngHorizon(Boid const& bi, Boid const& bj) {
-  double ang{0.};
-  V2D dist{bj.Pos() - bi.Pos()};
-  constexpr double PI{3.14159265358979323846264338327950288};
-  // First and second quadrant
-  if ((dist.x > 0. && dist.y < 0.) || (dist.x < 0. && dist.y < 0.)) {
-    ang = -std::atan2(dist.y, dist.x) * 180. / PI;
-  }  // Second and third quadrant
-  else if ((dist.x < 0. && dist.y > 0.) || (dist.x > 0. && dist.y > 0.)) {
-    ang = 360. - std::atan2(dist.y, dist.x) * 180. / PI;
-  } else if (dist.x > 0. && dist.y == 0.) {
-    ang = 0.;
-  } else if (dist.x == 0. && dist.y < 0.) {
-    ang = 90.;
-  } else if (dist.x < 0. && dist.y == 0.) {
-    ang = 180.;
-  } else if (dist.x == 0. && dist.y > 0.) {
-    ang = 270.;
-  } else if (dist.x == 0. && dist.y == 0.) {
-    ang = 0.;  // Dipende poi dalla condizione che impongo per l'aggiunta nel
-               // vettore
-  } else {
-    throw std::runtime_error(
-        "The position of the boids was not a compatible value to deduce an "
-        "angle with the horizon (counter-clockwise)");
-  }
-  return ang;
 }
 
 // Devi fare il test per quando la velocità è zero nel caso
@@ -101,7 +43,7 @@ void velocityChangeBoids(std::vector<Boid>& Boids, double dt,
   for (auto& bi : Boids) {
     std::vector<Boid*> nearboids;
     V2D separation_vel;
-    V2D allignament_vel;
+    V2D alignment_vel;
     V2D cm_pos;
     V2D cohesion_vel;
     double gamma{normGammaHorizon(bi)};
@@ -152,7 +94,7 @@ void velocityChangeBoids(std::vector<Boid>& Boids, double dt,
           if (!(bi.IsPred()) && bj->IsPred()) {
             separation_vel += (bj->Pos() - bi.Pos()) * 2.;
 
-            allignament_vel -= bj->Vel();
+            alignment_vel -= bj->Vel();
             cm_pos -= bj->Pos();
           }
           // This is the update if bi is a predator and bj a predator
@@ -160,7 +102,7 @@ void velocityChangeBoids(std::vector<Boid>& Boids, double dt,
             if (distSq(bi.Pos(), bj->Pos()) < danger_rad_sq) {
               separation_vel += bj->Pos() - bi.Pos();
             }
-            allignament_vel += bj->Vel();
+            alignment_vel += bj->Vel();
             cm_pos += bj->Pos();
           }
           // This is the update if bi is a prey and bj a predator
@@ -168,20 +110,19 @@ void velocityChangeBoids(std::vector<Boid>& Boids, double dt,
             if (distSq(bi.Pos(), bj->Pos()) < danger_rad_sq) {
               separation_vel += bj->Pos() - bi.Pos();
             }
-            allignament_vel += bj->Vel();
+            alignment_vel += bj->Vel();
             cm_pos += bj->Pos();
           }
         }
       }
       separation_vel = -params.separation * separation_vel;
-      allignament_vel =
-          params.allignment * (invNear * allignament_vel - bi.Vel());
+      alignment_vel = params.allignment * (invNear * alignment_vel - bi.Vel());
       cm_pos = invNear * cm_pos;
       cohesion_vel = params.cohesion * (cm_pos - bi.Pos());
     }
     // If there are no near boids it will update anyway but vup will be 0
     // because of the standard inizialization of the V2D
-    V2D vup{separation_vel + allignament_vel + cohesion_vel};
+    V2D vup{separation_vel + alignment_vel + cohesion_vel};
     bi.update(dt, flock_window_size_d, vup, params.toroidal);
   }
 }
